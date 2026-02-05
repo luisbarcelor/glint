fa0bf7ba54ee196052dafc6b271e90313c25c240
Fran_Unmei <franjlucuixv@gmail.com>
2026-02-05 13:59:35 +0100

    fix(upload): register IVideoUploadService to resolve DI error

--- backend/Glint.HttpApi/Program.cs
+++ backend/Glint.HttpApi/Program.cs
@@ -1,3 +1,5 @@
+using Glint.HttpApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
 
 builder.Services.AddControllers();
+builder.Services.AddScoped<IVideoUploadService, VideoUploadService>();
 
 var app = builder.Build();
 
